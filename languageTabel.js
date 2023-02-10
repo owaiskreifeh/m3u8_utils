56a5d2e66b0098ba6523ca88294abb138d42dd02
owaiskreifeh <owais.abukreefeh@shahid.net>
2023-02-10 18:57:17 +0300

    chore: update language table

---
+++ languageTabel.js
@@ -0,0 +1,410 @@
+module.exports = [
+    {
+        "language": "audioname",
+        "translation": {
+            "ar": "الأصلي",
+            "en": "Original",
+            "fr": "Version originale"
+        }
+    },
+    {
+        "language": "Original",
+        "translation": {
+            "ar": "الأصلي",
+            "en": "Original",
+            "fr": "Version originale"
+        }
+    },
+    {
+        "language": "off",
+        "translation": {
+            "ar": "ايقاف",
+            "en": "Off",
+            "fr": "Désactivé"
+        }
+    },
+    {
+        "language": "ar",
+        "translation": {
+            "ar": "العربيّة",
+            "en": "Arabic",
+            "fr": "Arabe"
+        }
+    },
+    {
+        "language": "ar_dz",
+        "translation": {
+            "ar": "العربيّة (الجزائر)",
+            "en": "Arabic (Algeria)",
+            "fr": "Arabe (Algérie)"
+        }
+    },
+    {
+        "language": "ar_bh",
+        "translation": {
+            "ar": "العربيّة (البحرين)",
+            "en": "Arabic (Bahrain)",
+            "fr": "Arabe (Bahreïn)"
+        }
+    },
+    {
+        "language": "ar_eg",
+        "translation": {
+            "ar": "العربيّة (مصر)",
+            "en": "Arabic (Egypt)",
+            "fr": "Arabe (Égypte)"
+        }
+    },
+    {
+        "language": "ar_iq",
+        "translation": {
+            "ar": "العربيّة (العراق)",
+            "en": "Arabic (Iraq)",
+            "fr": "Arabe (Irak)"
+        }
+    },
+    {
+        "language": "ar_jo",
+        "translation": {
+            "ar": "العربيّة (الأردن)",
+            "en": "Arabic (Jordan)",
+            "fr": "Arabe (Jordanie)"
+        }
+    },
+    {
+        "language": "ar_kw",
+        "translation": {
+            "ar": "العربيّة (الكويت)",
+            "en": "Arabic (Kuwait)",
+            "fr": "Arabe (Koweït)"
+        }
+    },
+    {
+        "language": "ar_lb",
+        "translation": {
+            "ar": "العربيّة (لبنان)",
+            "en": "Arabic (Lebanon)",
+            "fr": "Arabe (Liban)"
+        }
+    },
+    {
+        "language": "ar_ly",
+        "translation": {
+            "ar": "العربيّة (ليبيا)",
+            "en": "Arabic (Libya)",
+            "fr": "Arabe (Libye)"
+        }
+    },
+    {
+        "language": "ar_ma",
+        "translation": {
+            "ar": "العربيّة (المغرب)",
+            "en": "Arabic (Morocco)",
+            "fr": "Arabe (Maroc)"
+        }
+    },
+    {
+        "language": "ar_om",
+        "translation": {
+            "ar": "العربيّة (عُمان)",
+            "en": "Arabic (Oman)",
+            "fr": "Arabe (Oman)"
+        }
+    },
+    {
+        "language": "ar_qa",
+        "translation": {
+            "ar": "العربيّة (قطر)",
+            "en": "Arabic (Qatar)",
+            "fr": "Arabe (Qatar)"
+        }
+    },
+    {
+        "language": "ar_sa",
+        "translation": {
+            "ar": "العربيّة (المملكة العربيّة السعودية)",
+            "en": "Arabic (Saudi Arabia)",
+            "fr": "Arabe (Arabie saoudite)"
+        }
+    },
+    {
+        "language": "ar_sy",
+        "translation": {
+            "ar": "العربيّة (سوريا)",
+            "en": "Arabic (Syria)",
+            "fr": "Arabe (Syrie)"
+        }
+    },
+    {
+        "language": "ar_tn",
+        "translation": {
+            "ar": "العربيّة (تونس)",
+            "en": "Arabic (Tunisia)",
+            "fr": "Arabe (Tunisie)"
+        }
+    },
+    {
+        "language": "ar_ae",
+        "translation": {
+            "ar": "العربيّة (الإمارات العربيّة المتحدة)",
+            "en": "Arabic (U.A.E.)",
+            "fr": "Arabe (Émirats arabes unis)"
+        }
+    },
+    {
+        "language": "zh_cn",
+        "translation": {
+            "ar": "الصينية",
+            "en": "Chinese",
+            "fr": "Chinois"
+        }
+    },
+    {
+        "language": "da",
+        "translation": {
+            "ar": "الدنماركية",
+            "en": "Danish",
+            "fr": "Danois"
+        }
+    },
+    {
+        "language": "nl_be",
+        "translation": {
+            "ar": "الهولندية",
+            "en": "Dutch",
+            "fr": "Néerlandais"
+        }
+    },
+    {
+        "language": "en",
+        "translation": {
+            "ar": "الإنجليزية",
+            "en": "English",
+            "fr": "Anglais"
+        }
+    },
+    {
+        "language": "en_au",
+        "translation": {
+            "ar": "الإنجليزية (أستراليا)",
+            "en": "English (Australia)",
+            "fr": "Anglais (Australie)"
+        }
+    },
+    {
+        "language": "en_gb",
+        "translation": {
+            "ar": "الإنجليزية (المملكة المتحدة)",
+            "en": "English (United Kingdom)",
+            "fr": "Anglais (Royaume-Uni)"
+        }
+    },
+    {
+        "language": "en_us",
+        "translation": {
+            "ar": "الإنجليزية (الولايات المتحدة)",
+            "en": "English (United States)",
+            "fr": "Anglais (États-Unis)"
+        }
+    },
+    {
+        "language": "fa",
+        "translation": {
+            "ar": "الفارسية",
+            "en": "Farsi",
+            "fr": "Persan"
+        }
+    },
+    {
+        "language": "fr_ca",
+        "translation": {
+            "ar": "الفرنسية (كندا)",
+            "en": "French (Canada)",
+            "fr": "Français (Canada)"
+        }
+    },
+    {
+        "language": "fr",
+        "translation": {
+            "ar": "الفرنسية",
+            "en": "French",
+            "fr": "Français"
+        }
+    },
+    {
+        "language": "de",
+        "translation": {
+            "ar": "الألمانية",
+            "en": "German",
+            "fr": "Allemand"
+        }
+    },
+    {
+        "language": "el",
+        "translation": {
+            "ar": "اليونانية",
+            "en": "Greek",
+            "fr": "Grec"
+        }
+    },
+    {
+        "language": "hi",
+        "translation": {
+            "ar": "الهندية",
+            "en": "Hindi",
+            "fr": "Hindi"
+        }
+    },
+    {
+        "language": "it",
+        "translation": {
+            "ar": "الإيطالية",
+            "en": "Italian ",
+            "fr": "Italien"
+        }
+    },
+    {
+        "language": "ja",
+        "translation": {
+            "ar": "اليابانية",
+            "en": "Japanese",
+            "fr": "Japonais"
+        }
+    },
+    {
+        "language": "ko",
+        "translation": {
+            "ar": "الكورية",
+            "en": "Korean",
+            "fr": "Coréen"
+        }
+    },
+    {
+        "language": "ku",
+        "translation": {
+            "ar": "الكردية",
+            "en": "Kurdish",
+            "fr": "Kurde"
+        }
+    },
+    {
+        "language": "ru",
+        "translation": {
+            "ar": "الروسية",
+            "en": "Russian",
+            "fr": "Russe"
+        }
+    },
+    {
+        "language": "es",
+        "translation": {
+            "ar": "الإسبانية",
+            "en": "Spanish ",
+            "fr": "Espagnol"
+        }
+    },
+    {
+        "language": "tr",
+        "translation": {
+            "ar": "التركية",
+            "en": "Turkish",
+            "fr": "Turc"
+        }
+    },
+    {
+        "language": "uk",
+        "translation": {
+            "ar": "الأوكرانية",
+            "en": "Ukrainian",
+            "fr": "Ukrainien"
+        }
+    },
+    {
+        "language": "ur",
+        "translation": {
+            "ar": "الأردية",
+            "en": "Urdu",
+            "fr": "Ourdou"
+        }
+    },
+    {
+        "language": "pa",
+        "translation": {
+            "ar": "الباكستانية",
+            "en": "Pakistani",
+            "fr": "Pendjabi"
+        }
+    },
+    {
+        "language": "pt",
+        "translation": {
+            "ar": "البرتغالية",
+            "en": "Portuguese",
+            "fr": "Portugais"
+        }
+    },
+    {
+        "language": "sv",
+        "translation": {
+            "ar": "السويدية",
+            "en": "Swedish",
+            "fr": "Suédois"
+        }
+    },
+    {
+        "language": "ar_cl",
+        "translation": {
+            "ar": "العربية - مدبلجة",
+            "en": "Arabic - Dubbed",
+            "fr": "Arabe - Dubbed"
+        }
+    },
+    {
+        "language": "qae",
+        "translation": {
+            "ar": "dummy_subs_qae",
+            "en": "dummy_subs_qae",
+            "fr": "dummy_subs_qae"
+        }
+    },
+    {
+        "language": "qor",
+        "translation": {
+            "ar": "dummy_audio_qor",
+            "en": "dummy_audio_qor",
+            "fr": "dummy_audio_qor"
+        }
+    },
+    {
+        "language": "qaa",
+        "translation": {
+            "ar": "dummy_audio_qaa",
+            "en": "dummy_audio_qaa",
+            "fr": "dummy_audio_qaa"
+        }
+    },
+    {
+        "language": "qab",
+        "translation": {
+            "ar": "dummy_audio_qab",
+            "en": "dummy_audio_qab",
+            "fr": "dummy_audio_qab"
+        }
+    },
+    {
+        "language": "qac",
+        "translation": {
+            "ar": "dummy_audio_qac",
+            "en": "dummy_audio_qac",
+            "fr": "dummy_audio_qac"
+        }
+    },
+    {
+        "language": "qad",
+        "translation": {
+            "ar": "dummy_subs_qad",
+            "en": "dummy_subs_qad",
+            "fr": "dummy_subs_qad"
+        }
+    }
+]
